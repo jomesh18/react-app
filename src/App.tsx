6e2be509dc95058a99116e3515e0c5ac88fa33a9
jomesh18 <42250958+jomesh18@users.noreply.github.com>
2024-10-26 17:13:25 +0530

    feat completed creating custom data fetching hook

--- src/App.tsx
+++ src/App.tsx
@@ -205,31 +205,11 @@
 
 // export default App;
 
-import { useEffect, useState } from "react";
-import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 const App = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((response) => {
-        setUsers(response.data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setIsLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (userToDelete: User) => {
     const originalUsers = [...users];
